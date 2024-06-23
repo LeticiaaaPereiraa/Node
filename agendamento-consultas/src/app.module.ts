69c06159db748d8d006b708ffd4298da6c5f8f7f
TiagoBuzelii <tiago.buzeli@outlook.com>
2024-06-22 21:04:57 -0300

    Modificacao e correcao

--- agendamento-consultas/src/app.module.ts
+++ agendamento-consultas/src/app.module.ts
@@ -1,11 +1,19 @@
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgendamentoModule } from './agendamento/agendamento.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(), 
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'banco_hackathon',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     AgendamentoModule,
   ],
   controllers: [],
